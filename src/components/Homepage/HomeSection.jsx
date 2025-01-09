a0d17a7eae08474f96c294e67b2fab3bf7ab9130
ahmedyehia33 <ahmed.scott73@gmail.com>
2025-01-09 19:08:15 +0200

    updating header

--- src/components/Homepage/HomeSection.jsx
+++ src/components/Homepage/HomeSection.jsx
@@ -21,19 +21,19 @@ const HomeSection = () => {
           </video>
 
           {/* Content */}
-          <div className="relative z-10 w-[70%]   flex flex-col items-center content-center justify-center">
+          <div className="relative z-10 w-full   flex flex-col items-center content-center justify-center">
               <div
                   id="logo"
-                  className="w-[100%]  sm:w-[60%] md:w-[50%] flex items-center justify-center content-center"
+                  className="w-full  sm:w-[100%] md:w-[60%]  flex items-center justify-center content-center"
               >
                   <img
-                      src="/logo-02.jpg"
+                      src="/logos/FOR WHITE BACKGROUND2.png"
                       alt="monopoly-logo"
-                      className="w-full opacity-70 h-[80%]"
+                      className="w-full  h-[100%]"
                   />
               </div>
               <div>
-                  <h1 className="text-4xl font-bold text-center text-gray-900">
+                  <h1 className="text-4xl md:text-6xl font-bold text-center text-black  ">
                       <Typewriter
                           onInit={(typewriter) => {
                               typewriter
