101151855c81508b306423a3fa8695b78e76a60e
ahmedyehia33 <ahmed.scott73@gmail.com>
2025-01-11 23:14:01 +0200

    add work with us section

--- src/components/Homepage/WorkWithUs.jsx
+++ src/components/Homepage/WorkWithUs.jsx
@@ -3,8 +3,96 @@ import React from 'react';
 const WorkWithUs = () => {
     return ( 
             <>
-                <div className='w-full h-[30rem]  bg-white text-5xl text-black text-center'>
-                    Work With US
+                <div className='w-full   bg-white flex justify-center py-[2.5rem] '>
+                    <div className='w-[94%] bg-gray-300 px-5 flex flex-col pt-16 gap-10 rounded-3xl'>
+                        <div className='w-full text-center flex-col justify-center'>
+                            <h1 className='text-3xl sm:text-6xl tracking-wide sm:tracking-widest font-extrabold'>
+                            Why Work with Us?
+                            </h1>
+                        </div>
+                        <div id='work-with-us-content' className='flex flex-col gap-8'>
+                            <div className='flex flex-col md:flex-row md:h-[auto] items-baseline'>
+                                <div className='flex flex-col gap-5 text-center w-[90%] md:w-[35%] md:h-[full] lg:h-[18rem] p-5 md:border-r-2 md:border-black md:border-opacity-40'>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Expertise in the UAE Real Estate Market 
+                                    </h2>
+                                    <p className='text-xl'>
+                                        With deep knowledge of the local market, we offer tailored advice and solutions for buyers, sellers, and investors, ensuring your real estate journey is seamless and profitable.
+                                    </p>
+                                </div>
+                                <div className='flex flex-col text-center gap-5 w-[90%] md:w-[35%] md:h-[100%] p-2 md:border-r-2 md:border-black md:border-opacity-40'>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Customer-Centric Approach
+                                    </h2>
+                                    <p className='text-xl'>
+                                       Your goals are our priority. We focus on understanding your needs and providing personalized services to help you find the perfect property or investment.
+                                    </p>
+                                </div>
+                                <div className='flex flex-col text-center gap-5 w-[90%] md:w-[35%] md:h-[18rem] p-2 '>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Transparency and Trust
+                                    </h2>
+                                    <p className='text-xl'>
+                                      We believe in building long-term relationships with our clients through honesty, integrity, and full transparency in all our transactions.
+                                    </p>
+                                </div>
+                                </div>
+                                <div className='flex flex-col md:flex-row md:h-[auto] items-baseline'>
+                                <div className='flex flex-col gap-5 text-center w-[90%] md:w-[35%] md:h-[full] lg:h-[18rem] p-5 md:border-r-2 md:border-black md:border-opacity-40'>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Innovative Solutions
+                                    </h2>
+                                    <p className='text-xl'>
+                                        Leveraging cutting-edge technology and data analytics, we provide insights that give you a competitive advantage, whether you’re buying, selling, or investing.
+                                    </p>
+                                </div>
+                                <div className='flex flex-col gap-5 text-center w-[90%] md:w-[35%] md:h-[full] lg:h-[18rem] p-5 md:border-r-2 md:border-black md:border-opacity-40'>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Wide Network of Opportunities
+                                    </h2>
+                                    <p className='text-xl'>
+                                         We connect you to an extensive portfolio of properties, from luxury apartments in Dubai to emerging investment opportunities across the Emirates.
+                                    </p>
+                                </div>
+                                <div className='flex flex-col gap-5 text-center w-[90%] md:w-[35%] md:h-[full] lg:h-[18rem] p-5 '>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Growth Potential
+                                    </h2>
+                                    <p className='text-xl'>
+                                         Partnering with us means joining a company that’s on the rise. As a startup, we’re agile, forward-thinking, and focused on creating value for our clients and investors.
+                                    </p>
+                                </div>
+                                    
+                                </div>
+                                <div className='flex flex-col md:flex-row md:h-[auto] items-baseline'>
+                                <div className='flex flex-col gap-5 text-center w-[90%] md:w-[50%] md:h-[full] lg:h-[18rem] p-5 md:border-r-2 md:border-black md:border-opacity-40'>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Sustainability Focus
+                                    </h2>
+                                    <p className='text-xl'>
+                                       We are committed to promoting sustainable real estate practices, helping you invest in properties that align with environmental and societal goals.
+                                    </p>
+                                </div>
+                                <div className='flex flex-col gap-5 text-center w-[90%] md:w-[45%] md:h-[full] lg:h-[18rem] p-5 '>
+                                    <h2 className='text-2xl font-semibold'>
+                                        Your Success is Our Success
+                                    </h2>
+                                    <p className='text-xl'>
+                                         We measure our success by how well we help you achieve your real estate goals. Your satisfaction drives everything we do.
+                                    </p>
+                                </div>
+                              
+                                    
+                                </div>
+                            
+                            <div>
+
+                            </div>
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
      );
